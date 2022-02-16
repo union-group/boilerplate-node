805d980c2f3807ce8e6ad51d8f9222da285ba468
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-02-16 15:06:47 -0300

    feat: add errors handle

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,30 @@
 require('dotenv/config')
 
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 
-import { router } from './routes'
-
+import 'express-async-errors'
 import './database'
 
-export const app = express()
+import { AppError } from './errors/AppError'
+import { router } from './routes'
 
-const port = Number(process.env.PORT)
+export const app = express()
 
 app.use(express.json())
 
 app.use(router)
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+app.use(
+  (error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).json({
+        message: error.message,
+      })
+    }
+
+    return res.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${error.message}`,
+    })
+  },
+)
